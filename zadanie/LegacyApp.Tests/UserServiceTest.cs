21188a3ddf0acffdbfd991cacb5f04e2c397f064
shymcio <sbogusz03@gmail.com>
2024-03-30 20:42:47 +0100

    Refactored

---
+++ zadanie/LegacyApp.Tests/UserServiceTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using JetBrains.Annotations;
+using LegacyApp;
+using Xunit;
+
+namespace LegacyApp.Tests;
+
+[TestSubject(typeof(UserService))]
+public class UserServiceTest
+{
+
+    [Fact]
+    public void AddUser_Should_Return_False_When_First_Name_Is_Empty()
+    {
+        var userService = new UserService();
+        var result = userService.AddUser("", "Doe", "email@email.com", DateTime.Now, 1);
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_Should_Return_False_When_Last_Name_Is_Empty()
+    {
+        var userService = new UserService();
+        var result = userService.AddUser("John", "", "email@email.com", DateTime.Now, 1);
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_Should_Return_False_When_Email_Doesnt_Contain_At_Or_Dot()
+    {
+        var userService = new UserService();
+        var result = userService.AddUser("John", "Doe", "emailemail.com", DateTime.Now, 1);
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_Should_Return_False_When_Age_Less_Than_21_Years_Old()
+    {
+        var userService = new UserService();
+        var result = userService.AddUser("John", "Doe", "john.doe@example.com", DateTime.Now.AddYears(-20), 1);
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_Should_Return_True_When_Age_More_Than_21_Years_Old()
+    {
+        var userService = new UserService();
+        var result = userService.AddUser("John", "Doe", "john.doe@example.com", DateTime.Now.AddYears(22), 1);
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_Should_Return_True_When_Age_Equals_21_Years_Old()
+    {
+        var userService = new UserService();
+        var result = userService.AddUser("John", "Doe", "john.doe@example.com", DateTime.Now.AddYears(21), 1);
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_Should_Throw_Argument_Exception_When_Client_Doesnt_Exists()
+    {
+        var userService = new UserService();
+        Assert.Throws<ArgumentException>(() =>
+        {
+            userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("1900-03-21"), 100);
+        });
+    }
+    
+    [Fact]
+    public void AddUser_Should_Return_False_When_Client_Has_CreditLimit_Below_500()
+    {
+        var userService = new UserService();
+        var result = userService.AddUser("John", "Kowalski", "johndoe@gmail.com", DateTime.Parse("1900-03-21"), 1);
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_Should_Return_True_When_Client_Has_CreditLimit_Above_500()
+    {
+        var userService = new UserService();
+        var result = userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("1900-03-21"), 4);
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public void AddUser_Should_Return_True_When_Client_Is_Important()
+    {
+        var userService = new UserService();
+        var result = userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("1900-03-21"), 3);
+        Assert.True(result);
+    }
+}
---
+++ zadanie/LegacyApp/UserService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+        private ClientRepository _clientRepository = new ClientRepository();
+        private UserCreditService _userCreditService = new UserCreditService();
+        
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (IsFirstNameValid(firstName) || IsLastNameValid(lastName))
+            {
+                return false;
+            }
+
+            if (CheckEmail(email))
+            {
+                return false;
+            }
+
+            var age = CalculateAgeFromBirthDate(dateOfBirth);
+
+            if (age < 21)
+            {
+                return false;
+            }
+
+            var client = _clientRepository.GetById(clientId);
+
+            var user = new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            if (client.Type == "VeryImportantClient")
+            {
+                user.HasCreditLimit = false;
+            }
+            else if (client.Type == "ImportantClient")
+            {
+                user.HasCreditLimit = true;
+                
+                int creditLimit = _userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth); 
+                user.CreditLimit = creditLimit * 2;
+            }
+            else
+            {
+                user.HasCreditLimit = true;
+                
+                int creditLimit = _userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth); 
+                user.CreditLimit = creditLimit;
+            }
+
+            if (user.HasCreditLimit && user.CreditLimit < 500)
+            {
+                return false;
+            }
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        private static int CalculateAgeFromBirthDate(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            
+            var birthMonthBiggerThanCurrentMonth = now.Month < dateOfBirth.Month;
+            var birthMonthAndCurrentMonthSame = now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day;
+            if (birthMonthBiggerThanCurrentMonth || birthMonthAndCurrentMonthSame) age--;
+            return age;
+        }
+
+        public static bool CheckEmail(string email)
+        {
+            return !email.Contains("@") && !email.Contains(".");
+        }
+
+        public static bool IsFirstNameValid(string firstName)
+        {
+            return string.IsNullOrEmpty(firstName);
+        }
+        
+        public static bool IsLastNameValid(string lastName)
+        {
+            return string.IsNullOrEmpty(lastName);
+        }
+    }
+}
